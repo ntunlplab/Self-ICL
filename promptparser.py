734e7b363a557d78c512c523389ec6c40c529b47
Brian-Ckwu <sonny40920710@gmail.com>
2023-05-18 08:06:08 +0800

    feat(ckwu): add extract_pred(.) to parse prediction results

--- promptparser.py
+++ promptparser.py
@@ -12,6 +12,12 @@ class PromptParser(object):
             raise ValueError(f"Number of demos in full_demo_inputs ({len(sep_demo_inputs)}) does not match num_demos ({self._num_demos})")
         return sep_demo_inputs
     
+    def extract_pred(self, full_res: str):
+        pred = re.findall(pattern=r"A:\W*(\w+?)\W*\Z", string=full_res)
+        if len(pred) != 1:
+            raise ValueError(f"Number of predictions in full_res ({len(pred)}) is not 1")
+        return pred[0]
+    
 # unit tests
 if __name__ == "__main__":
     num_demos = 3
