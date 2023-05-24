fb8d0cf1335f719ffc8d45c5c40ba62b240fd94f
Brian-Ckwu <sonny40920710@gmail.com>
2023-05-25 07:40:13 +0800

    feat(ckwu): add comments and comparison scripts

--- log/significance/mcnemar.py
+++ log/significance/mcnemar.py
@@ -1,7 +1,15 @@
+import json
+from pathlib import Path
 from typing import Dict, List
 from statsmodels.stats.contingency_tables import mcnemar
 
 def compare_models(per_instance_A: Dict[str, List[int]], per_instance_B: Dict[str, List[int]], exact: bool, correction: bool) -> Dict[str, float]:
+    """
+    Suppose there are two models A and B.
+    Item B✘ B✔
+    A✘ [[a, b],
+    A✔  [c, d]]
+    """
     # check equal tasks
     for task_name in per_instance_A:
         if task_name not in per_instance_B:
@@ -31,3 +39,35 @@ def compare_models(per_instance_A: Dict[str, List[int]], per_instance_B: Dict[st
         res[task_name] = mcnemar(mcnemar_task, exact=exact, correction=correction).pvalue
     res["all"] = mcnemar(mcnemar_all, exact=exact, correction=correction).pvalue
     return res
+
+if __name__ == "__main__":
+    prefix_dir = "../model_outputs/stream"
+    suffix_file = "per_instance_100-testsize.json"
+
+    comps = {
+        "zero-shot": {
+            "A": "standard-zero-shot-class",
+            "B": "self-icl-no-cot-diverse-class"
+        },
+        "cot": {
+            "A": "standard-zero-shot-cot-class",
+            "B": "self-icl-cot-diverse-class"
+        },
+        "cot_vs_self_icl": {
+            "A": "standard-zero-shot-cot-class",
+            "B": "self-icl-no-cot-diverse-class"
+        }
+    }
+        
+    for comp_name, d in comps.items():
+        print(f"Setting: {comp_name}")
+        print(f"Comparing {d['A']} and {d['B']}")
+        A_path = (Path(prefix_dir) / d['A'] / suffix_file)
+        B_path = (Path(prefix_dir) / d['B'] / suffix_file)
+        
+        per_instance_A = json.loads(A_path.read_text())
+        per_instance_B = json.loads(B_path.read_text())
+        
+        res = compare_models(per_instance_A, per_instance_B, exact=False, correction=True)
+        print(f"p-value = {res['all']}")
+        print()
