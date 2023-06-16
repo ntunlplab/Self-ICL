1a67e6d0e3b6423ad179300e58e37da2d2e8d28b
Brian-Ckwu <sonny40920710@gmail.com>
2023-06-16 22:18:34 +0800

    feat(ckwu): add step3_stream functionality

--- experiment.py
+++ experiment.py
@@ -84,7 +84,8 @@ class Experiment(object):
         lacked_cases_path: Path = None,
         existing_demos_path: Path = None,
         random_pseudo_label: bool = False,
-        rerun_pseudo_label: bool = False
+        rerun_pseudo_label: bool = False,
+        step3_stream: bool = False
     ) -> None:
         # handle passed arguments
         if label_type and (label_type not in set(TaskGenerator.task2label_type.values())):
@@ -137,7 +138,7 @@ class Experiment(object):
             
             task = task_generator.get_task(task_name)
             add_parenthesis = list(task.label_set)[0][0] == '('
-            if (task.label_type in ["class", "choice"]) and (not self._config.use_cot) and (self._config.inference_mode == "stream"):
+            if (task.label_type in ["class", "choice"]) and (not self._config.use_cot) and ((self._config.inference_mode == "stream") or step3_stream):
                 label_set = task.label_set
             else:
                 label_set = None
@@ -251,7 +252,38 @@ class Experiment(object):
                                 shot = Shot(_input=demo_input, _label=demo_label)
                                 shots.append(shot)
                         else: # batch
-                            raise NotImplementedError
+                            raw_shots = (existing_demos_path / task_name / "demo-labels" / f"{i}.txt").read_text()
+                            if step3_stream:
+                                # parse shots to separate shots
+                                shots = []
+                                for k in range(1, self._config.num_demos + 1):
+                                    q_start = raw_shots.index(f"Q{k}:") + len(f"Q{k}:")
+                                    a_start = raw_shots.index(f"A{k}:") + len(f"A{k}:")
+                                    q_end = raw_shots.index(f"Q{k + 1}:") if k < self._config.num_demos else raw_shots.index("A1")
+                                    a_end = raw_shots.index(f"A{k + 1}:") if k < self._config.num_demos else len(raw_shots)
+                                    q = raw_shots[q_start:q_end].strip()
+                                    a = raw_shots[a_start:a_end].strip()
+                                    shot = Shot(_input=q, _label=a)
+                                    shots.append(shot)
+                                # inference each task input
+                                for j, task_input in enumerate(task_inputs):
+                                    print(f"Predicting batch #{i}-{j} (cot: {self.cot_check if self._config.use_cot else self.cot_cross}) -> ", end='')
+                                    prompt = StreamPrompt(
+                                        task_desc=task.task_desc,
+                                        inputs=task_input,
+                                        num_demos=self._config.num_demos,
+                                        shots=shots
+                                    )
+                                    pred_prompt = prompt.gen_prediction(cot=self._config.use_cot, add_parenthesis=add_parenthesis)
+                                    pred_prompt, res_text = self._model.complete(pred_prompt, label_set, temperature=self._config.temperature)
+                                    print(res_text)
+                                    full_text = pred_prompt + res_text
+                                    (task_log_path / "full-outputs" / f"{i * self._config.batch_size + j}.txt").write_text(full_text)
+                                # continue to next run (i.e., next batch)
+                                continue
+                            else:
+                                shots = raw_shots
+                                
                     # augment the original prompt with self-icl exemplars
                     if self._config.inference_mode == "stream":
                         # update prompt to augmented prompt
@@ -289,7 +321,8 @@ class Experiment(object):
     def evaluate(
         self,
         label_type: str = None,
-        weighted_acc: bool = False
+        weighted_acc: bool = False,
+        step3_stream: bool = False
     ) -> None:
         # for generating task labels
         task_gen = TaskGenerator(
@@ -321,14 +354,14 @@ class Experiment(object):
             for i in range(num_runs):
                 if i < task.sample_size: # ensure i is within the sample size
                     # read inference result
-                    filename = f"{i // self._config.batch_size}.txt"
+                    filename = f"{i // (1 if step3_stream else self._config.batch_size)}.txt"
                     if self._config.exemplars_mode == "standard":
                         full_res = (task_log_path / filename).read_text()
                     else: # self-icl
                         full_res = (task_log_path / "full-outputs" / filename).read_text()
                     # parse inference result
                     label = task.get_new_labels().strip("()").upper()
-                    if self._config.inference_mode == "stream":
+                    if (self._config.inference_mode == "stream") or step3_stream:
                         pred = self._prompt_parser.extract_pred(full_res, use_cot=self._config.use_cot).strip("()").upper()
                     else: # batch
                         pred = self._prompt_parser.extract_pred_batch(full_res, answer_index=(i % self._config.batch_size) + 1 + self._config.num_demos).strip("()").upper()
@@ -379,6 +412,7 @@ def parse_args() -> Namespace:
     parser.add_argument("--existing_demos_path", type=Path, default=None)
     parser.add_argument("--random_pseudo_label", action="store_true")
     parser.add_argument("--rerun_pseudo_label", action="store_true")
+    parser.add_argument("--step3_stream", action="store_true")
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -389,7 +423,8 @@ if __name__ == "__main__":
     if args.eval:
         experiment.evaluate(
             label_type=args.label_type,
-            weighted_acc=args.weighted_acc
+            weighted_acc=args.weighted_acc,
+            step3_stream=args.step3_stream
         )
     else:
         experiment.run(
@@ -399,5 +434,6 @@ if __name__ == "__main__":
             lacked_cases_path=args.lacked_cases_path,
             existing_demos_path=args.existing_demos_path,
             random_pseudo_label=args.random_pseudo_label,
-            rerun_pseudo_label=args.rerun_pseudo_label
+            rerun_pseudo_label=args.rerun_pseudo_label,
+            step3_stream=args.step3_stream
         )
