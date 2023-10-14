798d7bed67c4a6a0178b0ded2fbec13cb809de87
Brian-Ckwu <sonny40920710@gmail.com>
2023-10-14 17:09:59 +0800

    feat: add PaLM API

--- model.py
+++ model.py
@@ -1,29 +1,40 @@
 import os
+import re
 import time
 import openai
 import tiktoken
+import google.generativeai as palm
+import google.api_core.exceptions
 from typing import Set, Tuple
-from prompt import StreamPrompt, BatchPrompt
+from prompt import StreamPrompt
 from colorama import Fore, Style
 from argparse import Namespace
 
 class Model(object):
     api_interval = 0.05 # seconds
     cost_per_1000tokens = 0.02
+    gpt_models = {"text-davinci-003"}
+    palm_models = {"models/text-bison-001"}
 
     def __init__(self, config: Namespace):
         self._config = config
-        self._tokenizer = tiktoken.encoding_for_model(self._config.model)
-        self._original_max_tokens = self._config.max_tokens
-        self._original_temperature = self._config.temperature
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        
+        if self._config.model in Model.gpt_models:
+            self._tokenizer = tiktoken.encoding_for_model(self._config.model)
+            self._original_max_tokens = self._config.max_tokens
+            self._original_temperature = self._config.temperature
+            openai.api_key = os.getenv("OPENAI_API_KEY")
+        else:
+            palm.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
     def retry_with_exponential_backoff(
         func,
         initial_delay: float = 0.5,
         exponential_base: float = 2,
         max_retries: int = 20,
-        errors: tuple = (openai.error.RateLimitError, openai.error.APIError),
+        errors: tuple = (
+            openai.error.RateLimitError, openai.error.APIError,
+            google.api_core.exceptions.ResourceExhausted, google.api_core.exceptions.ServiceUnavailable, google.api_core.exceptions.GoogleAPIError,
+        ),
     ):
         """Retry a function with exponential backoff."""
     
@@ -58,6 +69,14 @@ class Model(object):
     @retry_with_exponential_backoff
     def complete(self, prompt: str, label_set: Set[str] = set(), temperature: float = None) -> Tuple[str, str]:
         time.sleep(Model.api_interval)
+        if self._config.model in Model.gpt_models:
+            return self.gpt_complete(prompt, label_set, temperature)
+        elif self._config.model in Model.palm_models:
+            return self.palm_complete(prompt, label_set, temperature)
+        else:
+            raise Exception(Fore.RED + f"Model {self._config.model} not supported." + Style.RESET_ALL)
+
+    def gpt_complete(self, prompt: str, label_set: Set[str] = set(), temperature: float = None) -> Tuple[str, str]:
         params = vars(self._config)
         if label_set:
             max_tokens = 0
@@ -83,7 +102,36 @@ class Model(object):
             params["temperature"] = self._original_temperature
         print(Fore.GREEN + f" (Predicting with temperature: {params['temperature']}, max_tokens: {params['max_tokens']}) " + Style.RESET_ALL, end='')
         return prompt, openai.Completion.create(prompt=prompt, **params)["choices"][0]["text"]
-    
+
+    def palm_complete(self, prompt: str, label_set: Set[str] = set(), temperature: float = None) -> Tuple[str, str]:
+        params = {
+            "model": self._config.model,
+            "temperature": self._config.temperature if (temperature is None) else temperature,
+            "candidate_count": 1,
+            "top_p": self._config.top_p,
+            "max_output_tokens": self._config.max_tokens,
+        }
+        response = palm.generate_text(
+            **params,
+            prompt=prompt,
+            safety_settings=[  # set all safety settings to 4 (no restrictions)
+                {"category": "HARM_CATEGORY_DEROGATORY", "threshold": 4},
+                {"category": "HARM_CATEGORY_TOXICITY", "threshold": 4},
+                {"category": "HARM_CATEGORY_VIOLENCE", "threshold": 4},
+                {"category": "HARM_CATEGORY_SEXUAL", "threshold": 4},
+                {"category": "HARM_CATEGORY_MEDICAL", "threshold": 4},
+                {"category": "HARM_CATEGORY_DANGEROUS", "threshold": 4}
+            ]
+        )
+        print(Fore.GREEN + f" (Predicting with temperature: {params['temperature']}, max_tokens: {params['max_output_tokens']}) " + Style.RESET_ALL)
+        if response.result is None:
+            print(Fore.RED + f"Failed to generated new instance." + Style.RESET_ALL)
+            example_instance = re.findall(pattern=r"Example instance:\nQ:(.*?)\nNew instance 1:", string=prompt, flags=re.DOTALL)[0].strip()
+            result = f" {example_instance}\n\nNew instance 2:\nQ: {example_instance}\n\nNew instance 3:\nQ: {example_instance}"
+        else:
+            result = response.result
+        return prompt, result
+
     def set_api_key(self, key: str) -> None:
         openai.api_key = key
 
@@ -94,7 +142,7 @@ class Model(object):
 if __name__ == "__main__":
     # model
     config = Namespace(
-        model="text-davinci-003",
+        model="models/text-bison-001",
         max_tokens=1024,
         temperature=0.0,
         top_p=1.0,
